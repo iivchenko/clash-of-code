4bf8a52a63a4221581b06d61e0090a87ac1564eb
Ivan Ivcheno <ivan.ivchenko@citrix.om>
2018-01-02 12:38:35 +0200

    Logarithm clash

---
+++ ClashOfCode/Logarithm.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace ClashOfCode
+{
+    [TestFixture]
+    public sealed class Logarithm
+    {
+        [TestCase(1,1)]
+        public void Test(int n, int p)
+        {
+           Console.WriteLine(Math.Log(n, p));
+        }
+    }
+}
